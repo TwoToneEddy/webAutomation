973b5464827a49c25d5d55d0b9f3079b79fb3412
TwoToneEddy <lee.hudson1384@yahoo.co.uk>
2018-11-04 20:53:16 +0000

    Progress commit

--- webBot.py
+++ webBot.py
@@ -11,7 +11,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import re
+from selenium.webdriver.support.ui import Select
 
 class webBot(object):
 	def __init__(self):
@@ -29,25 +30,65 @@ class webBot(object):
 	def login(self):
 		# First login screen
 		# Get all elements needed
-		self.browser.get(self.local_variables['webSiteStr'])
-		surname=self.browser.find_element_by_name('surname')
-		sc1 = self.browser.find_element_by_name('sortCodeSet1')
-		sc2 = self.browser.find_element_by_name('sortCodeSet2')
-		sc3 = self.browser.find_element_by_name('sortCodeSet3')
-		acc_no = self.browser.find_element_by_name('accountNumber')
-		next_button = self.browser.find_element_by_id('Next')
 
-		# Input required credentials into fields
+		self.browser.get(self.local_variables['webSiteStr'])
+
+		surname = WebDriverWait(self.browser, 10).until(
+			EC.presence_of_element_located((By.ID, "surname0"))
+		)
+
+		card_no_rb = self.browser.find_element_by_xpath("//div[4]/div[1]/div[@class='row validation-combo' and 1]/span[@class='col-5-xl col-4-m col-6-s col-6-xs' and 1]/div[@class='radio-control' and 1]/label[@class='narrow' and 1]")
+		card_no_rb.click()
+		card_no_0 = self.browser.find_element_by_xpath("//input[@id='cardNumber0']")
+		card_no_1 = self.browser.find_element_by_xpath("//input[@id='cardNumber1']")
+		card_no_2 = self.browser.find_element_by_xpath("//input[@id='cardNumber2']")
+		card_no_3 = self.browser.find_element_by_xpath("//input[@id='cardNumber3']")
+		#surname = self.browser.find_element_by_id('surname0')
+		next_step_bt = self.browser.find_element_by_xpath("//button")
+
 		surname.send_keys(self.local_variables['surnameStr'])
-		sc1.send_keys(self.local_variables['sortcode1Str'])
-		sc2.send_keys(self.local_variables['sortcode2Str'])
-		sc3.send_keys(self.local_variables['sortcode3Str'])
-		acc_no.send_keys(self.local_variables['accountNoStr'])
-		next_button.click()
+		card_no_0.send_keys(self.local_variables['card0'])
+		card_no_1.send_keys(self.local_variables['card1'])
+		card_no_2.send_keys(self.local_variables['card2'])
+		card_no_3.send_keys(self.local_variables['card3'])
+		next_step_bt.click()
 
 		# Second login screen
-		passcode = self.browser.find_element_by_id('passcode')
-		passcode.send_keys(self.local_variables['passcode'])
+		mem_characters = WebDriverWait(self.browser, 10).until(
+			EC.presence_of_element_located((By.ID, "label-memorableCharacters"))
+		)
+
+		first_char =  mem_characters.text.split()[1][0]
+		second_char = mem_characters.text.split()[3][0]
+
+		first_char_menu = self.browser.find_element_by_xpath("//div[@class='dropdown firstMemorableCharacter']/div[@id='selectedCharacter' and @class='dropdown__selected ng-binding' and 1]")
+		first_char_menu.select_by_visible_text(self.local_variables['memorable'][int(first_char)])
+
+
+		print int(first_char)
+		print self.local_variables['memorable'][int(first_char)]
+		print mem_characters.text.split()
+
+		#surname=self.browser.find_element_by_name('surname')
+		#sc1 = self.browser.find_element_by_name('sortCodeSet1')
+		#sc2 = self.browser.find_element_by_name('sortCodeSet2')
+		#sc3 = self.browser.find_element_by_name('sortCodeSet3')
+		#acc_no = self.browser.find_element_by_name('accountNumber')
+		#next_button = self.browser.find_element_by_id('Next')
+
+		# Input required credentials into fields
+
+
+		#sc1.send_keys(self.local_variables['sortcode1Str'])
+		#sc2.send_keys(self.local_variables['sortcode2Str'])
+		#sc3.send_keys(self.local_variables['sortcode3Str'])
+		#acc_no.send_keys(self.local_variables['accountNoStr'])
+		#next_button.click()
+
+		# Second login screen
+		# passcode = self.browser.find_element_by_id('passcode')
+		#passcode = self.browser.find_element_by_xpath("//input[@name = 'passcode']")
+		#passcode.send_keys(self.local_variables['passcode'])
 
 
 def main():
